2c3f630aeb4271684cacb5dc83102d3cf9fd19d0
Andy Cheng <andy@foundi.info>
2025-07-24 11:32:30 +0800

    fix deprecation warning

--- rest_framework_mongoengine/fields.py
+++ rest_framework_mongoengine/fields.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils.encoding import smart_str
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 from bson.errors import InvalidId
